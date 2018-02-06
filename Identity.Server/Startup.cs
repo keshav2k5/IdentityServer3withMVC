ce0c824ffb68a412c81da51e0ed5566b93b939ac
keshav <keshav.rao@actlogica.com>
2018-02-06 23:26:16 +0530

    identity server setup with mvc project

--- Identity.Server/Startup.cs
+++ Identity.Server/Startup.cs
@@ -1,14 +1,61 @@
-﻿using Microsoft.Owin;
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+using IdentityServer3.Core.Configuration;
+using Microsoft.Owin;
 using Owin;
+using Identity.Server.Config;
+using IdentityServer3.Core.Models;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.OpenIdConnect;
+using Microsoft.IdentityModel.Protocols;
+using System.Configuration;
 
 [assembly: OwinStartupAttribute(typeof(Identity.Server.Startup))]
 namespace Identity.Server
 {
     public partial class Startup
     {
+        //public void Configuration(IAppBuilder app)
+        //{
+        //    ConfigureAuth(app);
+        //}
+
         public void Configuration(IAppBuilder app)
         {
-            ConfigureAuth(app);
+            app.Map("/identity", idsrvApp =>
+            {
+            idsrvApp.UseIdentityServer(new IdentityServerOptions
+            {
+                SiteName = "Embedded IdentityServer",
+                SigningCertificate = LoadCertificate(),
+
+                Factory = new IdentityServerServiceFactory()
+                            .UseInMemoryUsers(Users.Get())
+                            .UseInMemoryClients(Clients.Get())
+                            .UseInMemoryScopes(Scopes.Get())
+                });
+            });
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = "Cookies"
+            });
+
+            app.UseOpenIdConnectAuthentication(new OpenIdConnectAuthenticationOptions
+            {
+                Authority = "https://localhost:44396/identity",
+                ClientId = "mvc",
+                RedirectUri = "https://localhost:44396/",
+                ResponseType = "id_token",
+
+                SignInAsAuthenticationType = "Cookies"
+            });
+        }
+
+        private X509Certificate2 LoadCertificate()
+        {
+            var certificate = Convert.FromBase64String(ConfigurationManager.AppSettings["SigningCertificate"]);
+            return new X509Certificate2(certificate,"password");
+
         }
     }
 }
